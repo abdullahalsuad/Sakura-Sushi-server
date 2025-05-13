a117466c945d76da7e5da669157ad59376a23aed
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-05-13 23:01:06 +0600

    up

---
+++ index.js
@@ -0,0 +1,46 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
+
+// db
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@learning.jizv1xw.mongodb.net/?retryWrites=true&w=majority&appName=learning`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //
+  }
+}
+run().catch(console.dir);
