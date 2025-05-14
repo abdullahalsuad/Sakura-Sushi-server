8f7b329a6358ba988abc4a87168d73d6c1420b8c
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-05-14 09:00:49 +0600

    up

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -20,7 +20,6 @@ app.listen(port, () => {
 // db
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@learning.jizv1xw.mongodb.net/?retryWrites=true&w=majority&appName=learning`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -33,14 +32,40 @@ async function run() {
   try {
     // Connect the client to the server
     await client.connect();
+
+    // collection create
+    const database = client.db("sushidb");
+    const sushiCollection = database.collection("sushi");
+
+    // GET- All sushi
+    app.get("/sushi", async (req, res) => {
+      const cursor = sushiCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // POST - Add new sushi
+    app.post("/sushi", async (req, res) => {
+      console.log("Data received on server:", req.body);
+      const newSushi = req.body;
+      const result = await sushiCollection.insertOne(newSushi);
+      res.send(result);
+    });
+
+    // DELETE - A  sushi
+    app.delete("/sushi/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await sushiCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    //
   }
 }
 run().catch(console.dir);
