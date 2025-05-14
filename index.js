91366e9fe3d24369c0e6e3751fea467fe608aa81
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-05-14 17:08:48 +0600

    up

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server
-    await client.connect();
+    // await client.connect();
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
