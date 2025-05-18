2ecbe1ee92285afea781b28c50f8e21d8c7a6e80
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-05-18 11:50:17 +0600

    again deploy

--- src/server.js
+++ src/server.js
@@ -17,10 +17,6 @@ app.use(express.json());
 // Connect to DB and inject collection into model
 connectDB().then(async (db) => {
   await sushiModel.injectDB(db);
-
-  app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-  });
 });
 
 // Routes
@@ -29,4 +25,8 @@ app.use("/api", sushiRoutes);
 // Root route
 app.get("/", (req, res) => {
   res.send("Sushi server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
